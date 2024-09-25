06f6a3819aac71dd083e48fd464daac9c495369c
AZA <lognhor@gmail.com>
2024-09-25 14:58:45 +0300

    1.4.5.2

--- mainFile.kt
+++ mainFile.kt
@@ -1,7 +1,7 @@
 fun main() {
-    val trip1 = 3.20
-    val trip2 = 4.10
-    val trip3 = 1.72
-    val totalTripLength = trip1 + trip2 + trip3
-    println("$totalTripLength miles left to destination")
+    val nextMeeting = "Next meeting: "
+    val date = "January 1"
+    val place = " at work "
+    val reminder = nextMeeting + date + place
+    println(reminder)
 }
