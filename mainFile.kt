56a1154a260eaf280c007c8ab2a212b5454709f6
AZA <lognhor@gmail.com>
2024-09-25 15:02:49 +0300

    1.5.7

--- mainFile.kt
+++ mainFile.kt
@@ -1,10 +1,8 @@
 fun main() {
-    println(birthdayGreeting("Rover", 5))
-    println(birthdayGreeting(age = 2, name = "Rex"))
+    println(birthdayGreeting(age = 5))
+    println(birthdayGreeting(age = 2))
 }
 
-fun birthdayGreeting(name: String, age: Int): String {
-    val nameGreeting = "Happy Birthday, $name!"
-    val ageGreeting = "You are now $age years old!"
-    return "$nameGreeting\n$ageGreeting"
+fun birthdayGreeting(name: String = "Rover", age: Int): String {
+    return "Happy Birthday, $name! You are now $age years old!"
 }
