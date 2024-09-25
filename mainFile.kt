810bf73d99a7916a38b6e367284023a56633477a
AZA <lognhor@gmail.com>
2024-09-25 14:57:26 +0300

    1.4.4.2

--- mainFile.kt
+++ mainFile.kt
@@ -1,7 +1,8 @@
 fun main() {
-    var cartTotal = 0
-    println("Total: $cartTotal")
-
-    cartTotal = 20
-    println("Total: $cartTotal")
+    var count = 10
+    println("You have $count unread messages.")
+    count++
+    println("You have $count unread messages.")
+    count--
+    println("You have $count unread messages.")
 }
